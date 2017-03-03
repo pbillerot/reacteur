c08223fb536ee07db62c7b798797abc797331132
pbillerot <philippe.billerot@gmail.com>
2017-03-03 18:01:53 +0100

    Version 1.3.3

--- app/config/Dico.js
+++ app/config/Dico.js
@@ -22,7 +22,7 @@ const Dico = {
         title: 'REACTEUR',
         desc: "REACTEUR - Le Portail",
         url: 'https://github.com/pbillerot/reacteur',
-        copyright: 'build with REACTEUR 2016 - version 1.2.16',
+        copyright: 'build with REACTEUR 2016 - version 1.3.3',
     },
     apps: {
         tarot: {
